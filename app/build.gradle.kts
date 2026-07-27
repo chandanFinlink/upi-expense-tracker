6d6939e798e57643c1bcda6ea1c15f8d93d90914
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-07-27 15:11:57 +0530

    Add lifecycle and navigation compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,10 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+
+
 }
