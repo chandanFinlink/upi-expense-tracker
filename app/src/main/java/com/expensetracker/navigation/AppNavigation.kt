e69ba8d3c067537ddd42a540836f5a2215757a2d
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-07-27 17:03:02 +0530

    remaning creen creatd

--- app/src/main/java/com/expensetracker/navigation/AppNavigation.kt
+++ app/src/main/java/com/expensetracker/navigation/AppNavigation.kt
@@ -1,39 +1,124 @@
 package com.expensetracker.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.expensetracker.features.analytics.AnalyticsScreen
+import com.expensetracker.features.budgets.BudgetsScreen
 import com.expensetracker.features.home.HomeScreen
+import com.expensetracker.features.settings.SettingsScreen
+import com.expensetracker.features.transactions.TransactionsScreen
 
 @Composable
 fun AppNavigation() {
 
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = Routes.Home.route
-    ) {
+    Scaffold(
 
-        composable(Routes.Home.route) {
-            HomeScreen()
+        bottomBar = {
+
+            NavigationBar {
+
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+                val currentDestination = navBackStackEntry?.destination
+
+                BottomNavigationItems.forEach { item ->
+
+                    NavigationBarItem(
+
+                        selected = currentDestination?.hierarchy?.any {
+                            it.route == item.route
+                        } == true,
+
+                        onClick = {
+
+                            navController.navigate(item.route) {
+
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+
+                                launchSingleTop = true
+
+                                restoreState = true
+                            }
+
+                        },
+
+                        icon = {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.title
+                            )
+                        },
+
+                        label = {
+                            Text(item.title)
+                        }
+
+                    )
+
+                }
+
+            }
+
         }
 
-        composable(Routes.Transactions.route) {
-            HomeScreen()
-        }
+    ) { padding ->
 
-        composable(Routes.Budgets.route) {
-            HomeScreen()
-        }
+        NavHost(
 
-        composable(Routes.Analytics.route) {
-            HomeScreen()
-        }
+            modifier = Modifier.padding(padding),
 
-        composable(Routes.Settings.route) {
-            HomeScreen()
+            navController = navController,
+
+            startDestination = Routes.Home.route
+
+        ) {
+
+            composable(Routes.Home.route) {
+
+                HomeScreen()
+
+            }
+
+            composable(Routes.Transactions.route) {
+
+                TransactionsScreen()
+
+            }
+
+            composable(Routes.Budgets.route) {
+
+                BudgetsScreen()
+
+            }
+
+            composable(Routes.Analytics.route) {
+
+                AnalyticsScreen()
+
+            }
+
+            composable(Routes.Settings.route) {
+
+                SettingsScreen()
+
+            }
+
         }
 
     }
