6421620f4607d16a9a0b8c5b51533b46cb3ec0eb
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-07-27 15:34:16 +0530

    Refactor build.gradle.kts for dependencies and plugins
    Removed redundant plugin declarations and updated activity-compose dependency version. Added Room Database dependencies back.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
 }
 
 
@@ -60,25 +57,26 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    
-    // Compose UI
+
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+
+    implementation("androidx.activity:activity-compose:1.9.0")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
     implementation("androidx.compose.material3:material3")
 
-    // Room Database
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
 
-    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.8.0")
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
+    ksp("androidx.room:room-compiler:2.6.1")
 }
