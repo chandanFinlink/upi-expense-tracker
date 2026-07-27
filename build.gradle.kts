282d40bcee38741b6bc5e14ba2e43851912c7433
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-07-27 12:49:59 +0530

    Change plugin application to false in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
 }
 
 android {
