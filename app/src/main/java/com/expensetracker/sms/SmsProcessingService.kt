981a532d12ebb73e4a102905535b7e9dff568c8c
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-07-28 10:48:25 +0530

    sms processing service

--- app/src/main/java/com/expensetracker/sms/SmsProcessingService.kt
+++ app/src/main/java/com/expensetracker/sms/SmsProcessingService.kt
@@ -1,23 +1,24 @@
 package com.expensetracker.sms
 
-import com.expensetracker.repository.TransactionRepository
 import com.expensetracker.parser.UpiSmsParser
+import com.expensetracker.repository.TransactionRepository
 
 class SmsProcessingService(
     private val repository: TransactionRepository
 ) {
 
- suspend fun processSms(
-    sender: String?,
-    body: String,
-    smsTime: Long
-) {
+    suspend fun processSms(
+        sender: String?,
+        body: String,
+        smsTime: Long
+    ) {
 
-        UpiSmsParser.parse(
-            smsBody = body,
-            sender = sender,
-            smsTime = smsTime
-        ) ?: return
+        val transaction =
+            UpiSmsParser.parse(
+                smsBody = body,
+                sender = sender,
+                smsTime = smsTime
+            ) ?: return
 
         val exists =
             repository.transactionExists(
