6e2df5792666341f03376e81b324a3fdddd1fb26
chandanFinlink <dev.chandanfinlink01@gmail.com>
2026-07-28 09:51:10 +0530

    permisisons

--- app/src/main/java/com/expensetracker/MainActivity.kt
+++ app/src/main/java/com/expensetracker/MainActivity.kt
@@ -1,26 +1,60 @@
 package com.expensetracker
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.expensetracker.navigation.AppNavigation
 import com.expensetracker.ui.theme.ExpenseTrackerTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val smsPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestSmsPermissions()
+
         setContent {
-
             ExpenseTrackerTheme {
-
                 AppNavigation()
-
             }
-
         }
-
     }
 
+    private fun requestSmsPermissions() {
+
+        val permissions = mutableListOf<String>()
+
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissions.add(Manifest.permission.READ_SMS)
+        }
+
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECEIVE_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissions.add(Manifest.permission.RECEIVE_SMS)
+        }
+
+        if (permissions.isNotEmpty()) {
+            smsPermissionLauncher.launch(
+                permissions.toTypedArray()
+            )
+        }
+    }
 }
